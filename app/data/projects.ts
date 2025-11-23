dec7147cc580efc48bf35e8f7f70751850beca5f
kouhei <kouhei.basketball7.8@gmail.com>
2025-11-23 17:01:13 +0900

    add - worksのデータを定義

---
+++ app/data/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: 'このWebサイト',
+    description: 'Next.jsやGSAPなどを使用したモダンなWebサイトです。',
+    tags: ['Next.js', 'TypeScript', 'Tailwind CSS', 'GSAP'],
+    github: 'https://github.com/kouheihayashi78/hk-web-portfolio',
+    demo: '#',
+  },
+  {
+    title: 'HKのブログ',
+    description: 'Quitaなどに載せたくない、私のポエムや独り言など...',
+    tags: ['Next.js', 'TypeScript', 'microCMS', 'Tailwind CSS'],
+    github: '#',
+    demo: '#',
+  },
+] as const
