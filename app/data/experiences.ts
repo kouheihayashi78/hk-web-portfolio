a8f2fb7edabca7987a9e55b2f2012d71345e81f8
kouhei <kouhei.basketball7.8@gmail.com>
2025-11-23 17:00:05 +0900

    add - 「経験」のオブジェクトデータのコミット漏れ

---
+++ app/data/experiences.ts
@@ -0,0 +1,40 @@
+export const experiences = [
+  {
+    type: 'work',
+    title: 'バックエンドエンジニア',
+    company: '広告系の会社',
+    period: '2025年4月 - 現在',
+    description:
+      'Web制作、WordPressの受託案件を中心に、クライアントのニーズに応じた柔軟な開発を行う',
+  },
+  {
+    type: 'work',
+    title: '色々やっていたエンジニア',
+    company: 'IT系ベンチャー',
+    period: '2022年3月 - 2025年1月',
+    description:
+      'SES企業にて、SES、受託、自社開発に従事。Laravel、Nextを用いたシステムの開発や、既存システムの保守・改善を担当',
+  },
+  {
+    type: 'education',
+    title: 'プログラミングスクール受講',
+    company: 'オンラインスクール',
+    period: '2021年4月 - 2021年12月',
+    description: 'Web開発の基礎からモダンな技術スタックまで学習。',
+  },
+  {
+    type: 'work',
+    title: '一般職',
+    company: '一般企業',
+    period: '2019年4月 - 2021年3月',
+    description: '電気工事会社やエステティック業界にて接客・営業業務に従事。',
+  },
+  {
+    type: 'education',
+    title: '情報デザイン学科',
+    company: '情報系の大学',
+    period: '2015年 - 2019年',
+    description:
+      '学生時代はプログラミング、動画制作、アニメーション制作など諸々の分野を学びましたが、クリエイティブな思考とITスキルの基礎はそんなに身につきませんでした。',
+  },
+]
